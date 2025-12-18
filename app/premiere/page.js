6c00002bc8f22ff0fc62a14447de249c0259bf88
bizleap1 <bizleap1@gmail.com>
2025-12-18 16:49:36 +0530

    update

--- app/premiere/page.js
+++ app/premiere/page.js
@@ -4,7 +4,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { motion } from "framer-motion";
 import { Italiana, Barlow } from "next/font/google";
-import Facilities from "../../components/Facilities";
+
 
 const italiana = Italiana({ weight: "400", subsets: ["latin"] });
 const barlow = Barlow({ weight: ["400", "600"], subsets: ["latin"] });
